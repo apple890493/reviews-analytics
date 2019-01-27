e3726f6650e4809cf1fe9de3f8d3491391f14608
Nono Lin <denkanono@gmail.com>
2019-01-27 18:58:15 +0800

    clac avg

--- read.py
+++ read.py
@@ -6,9 +6,12 @@ with open("reviews.txt", "r") as f:
 		count += 1#count = count +1
 		if count % 1000 == 0:#%是用來求餘數1001/1000==1 ; 1007/1000==7
 			print(len(data))
-print(len(data)) #讀取總共有幾則
-print(data[0])#索引標籤第一筆由0開始
-print("---------------------")
-print(data[1])
+print("檔案讀取完了,總共有", len(data), "筆資料")
 
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+
+print("留言的平均長度為", sum_len/len(data))
 
